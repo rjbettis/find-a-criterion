bcd23147cabdb7cc4041c53ba30f9c7ddef79e18
Ryan Bettis <rbettis2@asu.edu>
2020-06-03 20:46:48 -0700

    added more people

--- src/components/CombinedPeople.js
+++ src/components/CombinedPeople.js
@@ -1,7 +1,17 @@
 import IngmarBergman from '../data/people/ingmar_bergman.json';
 import RobertBressen from '../data/people/robert_bresson.json';
+import LuisBunuel from '../data/people/luis_bunuel.json';
+import HenriGeorgesClouzot from '../data/people/henri_georges_clouzot.json';
+import FedericoFellini from '../data/people/federico_fellini.json';
+import JeanLucGodard from '../data/people/jean_luc_godard.json';
+import AkiraKurosawa from '../data/people/akira_kurosawa.json';
 
 export default {
   ...IngmarBergman,
   ...RobertBressen,
+  ...LuisBunuel,
+  ...HenriGeorgesClouzot,
+  ...FedericoFellini,
+  ...JeanLucGodard,
+  ...AkiraKurosawa,
 };
