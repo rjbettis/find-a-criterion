d05824e192c074858743ab2f9369a229362cd57d
Ryan Bettis <rbettis2@asu.edu>
2020-06-18 23:18:33 -0700

    Fixed typos and grammar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -35,26 +35,27 @@ export class Home extends Component {
               website.
               <br />
               <br />
-              I have been wanting to make this site ever since Criterion.com
-              removed the explore section from their website. All of these lists
-              are still maintained but they are not put together in a convenient
-              way to browse through them on the new site. The only way to find
-              these lists now are through individual movies. If a movie belongs
-              to a theme it might say so at the bottom of the page (usually
-              doesn’t), or when you search for a movie and scroll down through
-              the results it will show the lists it belongs to. The site does
-              not have a page to leisurely browse through the 50+ lists they
-              have created. These are the reasons I wanted to make a site that
-              gives people a convenient way to browse these lists.
+              I have been wanting to make this site ever since Criterion removed
+              the "Explore" section from their website. All of these lists are
+              still maintained but they are not put together in a convenient way
+              to browse through them on the new site. The only way to find these
+              lists now are through individual movies. If a movie belongs to a
+              theme it might say so at the bottom of the page (usually doesn’t),
+              or when you search for a movie and scroll down through the results
+              it will show the lists it belongs to. The site does not have a
+              page to leisurely browse through the 50+ theme/people lists. I
+              wanted to make a site that gives people a convenient way to browse
+              these lists.
               <br />
               <br />
               The lists themselves have also been changed in a few ways that
-              hinder their usefulness. They are now tied to their store, and as
-              a result they have been removing films as they go out of print.
-              They have also replaced individual films for box sets, which don’t
-              always make sense for every film in the set to be part of a theme.
-              For some time I was able to use Archive.org to look through them,
-              but as time went on the amount of new films missing added up.
+              hinder their usefulness. They are now tied to the Criterion.com
+              store, and as a result they have been removing films as they go
+              out of print. They have also replaced individual films for box
+              sets, which do not always make sense for every film in the set to
+              be part of a theme. For some time I was able to use Archive.org to
+              look through all these lists, but as time went on the amount of
+              new films missing added up.
               <br />
               <br />
               My plan for the future of this site is to expand it to include
@@ -63,10 +64,9 @@ export class Home extends Component {
               while maintaining the same kind of high curation standards that
               the Criterion Collection has. I would also like to implement a
               browse feature that lets you browse these films and see all the
-              lists they belong to.
-              <br />
-              <br />
-              <strong>Click to find a Criterion film to watch.</strong>
+              lists they belong to. I might end up linking the "Top 10" link to
+              the actual Criterion website because it has it's own section on
+              the site and is still useful.
             </Card.Text>
           </Card.Body>
           <Link to="/explore" className="stretched-link"></Link>
