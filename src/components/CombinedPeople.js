1ba8d235dbce1e60198c124b3960e3a09302ac90
Ryan Bettis <rbettis2@asu.edu>
2020-07-13 23:09:15 -0700

    added more people

--- src/components/CombinedPeople.js
+++ src/components/CombinedPeople.js
@@ -1,10 +1,17 @@
 import ChantalAkerman from '../data/people/chantal_akerman.json';
 import RobertAltman from '../data/people/robert_altman.json';
 import WesAnderson from '../data/people/wes_anderson.json';
+import MichelangeloAntonioni from '../data/people/michelangelo_antonioni.json';
+import JeanPaulBelmondo from '../data/people/jean_paul_belmondo.json';
 import IngmarBergman from '../data/people/ingmar_bergman.json';
 import RobertBressen from '../data/people/robert_bresson.json';
 import LuisBunuel from '../data/people/luis_bunuel.json';
+import JeanClaudeCarriere from '../data/people/jean_claude_carriere.json';
+import JohnCassavetes from '../data/people/john_cassavetes.json';
 import HenriGeorgesClouzot from '../data/people/henri_georges_clouzot.json';
+import JeanCocteau from '../data/people/jean_cocteau.json';
+import RaoulCoutard from '../data/people/raoul_coutard.json';
+
 import FedericoFellini from '../data/people/federico_fellini.json';
 import JeanLucGodard from '../data/people/jean_luc_godard.json';
 import AkiraKurosawa from '../data/people/akira_kurosawa.json';
@@ -13,10 +20,17 @@ export default {
   ...ChantalAkerman,
   ...RobertAltman,
   ...WesAnderson,
+  ...MichelangeloAntonioni,
+  ...JeanPaulBelmondo,
   ...IngmarBergman,
   ...RobertBressen,
   ...LuisBunuel,
+  ...JeanClaudeCarriere,
+  ...JohnCassavetes,
   ...HenriGeorgesClouzot,
+  ...JeanCocteau,
+  ...RaoulCoutard,
+
   ...FedericoFellini,
   ...JeanLucGodard,
   ...AkiraKurosawa,
